9e575c1ffd9c700eac79b6389c4e96d30528b38e
haruyq <156767674+haruyq@users.noreply.github.com>
2026-04-05 17:28:43 +0900

    bump up to 0.1.5

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 from typing import Callable, Any
 
 class Config:
-    __version__: str = "0.1.4"
+    __version__: str = "0.1.5"
     
     BASE_URL: str = "http://example.com"
     STT_ENGINE: str = "edgestt"
